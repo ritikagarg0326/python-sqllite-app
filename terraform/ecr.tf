53637161c4b05ba3186dee9700d7432856c7d50d
Ritika Garg <ritikagarg60814@gmail.com>
2026-05-25 22:34:55 +0530

    updated ECR with image scanning, encryption and tagging

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,9 +1,22 @@
 resource "aws_ecr_repository" "flask_repo" {
-  name = "flask-app"
+
+  name                 = "flask-app"
+
+  image_tag_mutability = "MUTABLE"
+
+  force_delete         = true
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
-  force_delete = true
+  tags = {
+    Name        = "flask-app-ecr"
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
