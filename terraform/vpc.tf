e87105c5159ba2811c978060b2a22e3756690ec7
Ritika Garg <ritikagarg60814@gmail.com>
2026-05-26 23:09:15 +0530

    created a multi-AZ VPC with separate public and private subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,15 +1,62 @@
 module "vpc" {
+
   source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
 
   name = "eks-vpc"
+
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  ########################################
+  # NAT Gateway
+  ########################################
 
   enable_nat_gateway = true
+
   single_nat_gateway = true
 
+  ########################################
+  # DNS Support
+  ########################################
+
   enable_dns_hostnames = true
+
+  enable_dns_support = true
+
+  ########################################
+  # EKS REQUIRED TAGS
+  ########################################
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  ########################################
+  # Common Tags
+  ########################################
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+    Project     = "eks-cluster"
+  }
 }
